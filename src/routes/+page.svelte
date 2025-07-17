86c4817d3ac7bd986f6fa460f2cf0e5a24e54459
nebmit <76664673+nebmit@users.noreply.github.com>
2025-07-17 20:55:55 +0000

    feat: create sveltekit skeleton app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { hello_world } from "$lib/paraglide/messages";
+    import { setLocale } from "$lib/paraglide/runtime";
+</script>
+
+<h1>{hello_world({ name: "World" })}</h1>
+<br>
+<button on:click={() => setLocale("en")}> English </button>
+<button on:click={() => setLocale("ja")}> Japanese </button>
